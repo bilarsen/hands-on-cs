fd8b666057d932c45323eeface57613babf6cefa
cypherman <aliev@element-soft.com>
2021-09-09 00:16:54 +0300

    implement learning cycle for Neuron

--- src/main/java/deeplearning/Neuron.java
+++ src/main/java/deeplearning/Neuron.java
@@ -2,31 +2,49 @@ package deeplearning;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Neuron {
 
     private final int[][] synapses;
 
-    private final int boundary;
+    private int boundary;
 
-    public Neuron(int n, int m, int boundary) {
+    public Neuron(int n, int m) {
         this.synapses = new int[n][m];
-        this.boundary = boundary;
     }
 
     public int activationFunction(File file) {
         int sum = 0;
+        this.boundary = 35;
         try (Scanner scanner = new Scanner(file)) {
             for (int i = 0; i < synapses.length; i++) {
                 for (int j = 0; j < synapses[i].length; j++) {
                     sum += scanner.nextInt() * synapses[i][j];
                 }
             }
-            return sum < boundary ? 1 : 0;
+            return sum < boundary ? 0 : 1;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             return 0;
         }
+    }
+
+    public void learn(List<File> files, int cycleCount) {
+        for (int cycle = 0; cycle < cycleCount; cycle++) {
+            for (File file : files) {
+                boolean correctFile = file.getName().contains("positive");
+                int result = activationFunction(file);
+                if (correctFile && !file.getName().startsWith("a")) lowerWeights(file);
+                else if (!correctFile && file.getName().startsWith("a")) increaseWeights(file);
+            }
+        }
+    }
+
+    private void increaseWeights(File file) {
+    }
+
+    private void lowerWeights(File file) {
     }
 }
